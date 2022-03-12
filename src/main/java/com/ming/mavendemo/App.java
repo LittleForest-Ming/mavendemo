998862fe0b8a723c9f0ba6434175688972aa1c69
guozhiming <2447549475@qq.com>
2022-03-12 10:47:57 +0800

    'test'

---
+++ src/main/java/com/ming/mavendemo/App.java
@@ -0,0 +1,13 @@
+package com.ming.mavendemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
